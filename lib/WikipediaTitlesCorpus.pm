0b1818756e24093cbed56207ad21e9ee991244a9
David Pavlík <david@dpavlik.cz>
2018-03-10 15:11:56 +0100

    Add Wikipedia-titles-only source

---
+++ lib/WikipediaTitlesCorpus.pm
@@ -0,0 +1,33 @@
+package WikipediaTitlesCorpus;
+
+use v5.10;
+use utf8;
+
+use Moose;
+use XML::SAX;
+use WikipediaXMLHandler;
+
+has 'filename' => (is => 'ro', isa => 'Str', required => 1);
+
+sub parse {
+    my $self = shift;
+    my $document_callback = shift;
+    my $end_callback = shift;
+
+    my $title = "";
+    my $parser = XML::SAX::ParserFactory->parser(
+      Handler => new WikipediaXMLHandler(
+        title_callback => $document_callback,
+        article_callback => sub {},
+        end_callback => $end_callback
+      )
+    );
+
+    open(my $fh, "<", $self->{filename});
+
+    $parser->parse_file($fh);
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
